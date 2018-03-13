92a2647e03015723c8b8cf37f9e61b9cf8191534
Oliver Ludwig <oludwig@hsr.ch>
2018-03-13 15:59:28 +0100

    added validation registration #3

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -15,15 +15,45 @@ class Signup extends React.Component<{}, *> {
     redirectToReferrer: false
   };
 
+//  inputValidation =  (String: input) => {
+//    const input = this.input;
+//    if(input.length === 0){
+//      return "Input is empty!";
+//    }
+//    if(input.length <= 3){
+//      return "Input is too short!";
+//    }
+//  }
+
   handleLoginChanged = (event: Event) => {
     if (event.target instanceof HTMLInputElement) {
       this.setState({ login: event.target.value });
     }
   };
 
+  errorForLogin = () => {
+    const login = this.state.login; 
+    if(login.length === 0){
+      return "Login is empty!";
+    } 
+    if(login.length <= 3){
+      return "Login is too short!";
+    } 
+  }
+
   handleFirstNameChanged = (event: Event) => {
     if (event.target instanceof HTMLInputElement) {
       this.setState({ firstname: event.target.value });
+    }
+  };
+ 
+  errorForFirstName = () => {
+    const firstName = this.state.firstname;
+    if(firstName.length === 0){
+      return "Firstname is empty!";
+    }
+    if (firstName.length <=3){
+      return"Firstname is too short!";
     }
   };
 
@@ -33,9 +63,29 @@ class Signup extends React.Component<{}, *> {
     }
   };
 
+  errorForLastName = () => {
+    const lastName = this.state.lastname;
+    if(lastName.length === 0){
+      return "Lastname is too empty!";
+    }
+    if(lastName.length <= 3){
+      return "Lastname is too short!";
+    }
+  }
+
   handlePasswordChanged = (event: Event) => {
     if (event.target instanceof HTMLInputElement) {
       this.setState({ password: event.target.value });
+    }
+  };
+
+  errorForPassword = () => {
+    const password = this.state.password;
+    if(password.length === 0){
+      return "Password is empty!"
+    }
+    if(password.length <= 3){
+      return "Password is too short!"
     }
   };
 
@@ -67,22 +117,26 @@ class Signup extends React.Component<{}, *> {
             placeholder="Login"
             value={this.state.login}
           />
+          <p>{this.errorForLogin()}</p>
           <input
             onChange={this.handleFirstNameChanged}
             placeholder="Vorname"
             value={this.state.firstname}
           />
+          <p>{this.errorForFirstName()}</p>
           <input
             onChange={this.handleLastNameChanged}
             placeholder="Nachname"
             value={this.state.lastname}
           />
+          <p>{this.errorForLastName()} </p>
           <input
             onChange={this.handlePasswordChanged}
             placeholder="Passwort"
             type="password"
             value={this.state.password}
-          />
+          /> 
+          <p>{this.errorForPassword()}</p>
           <button onClick={this.handleSubmit}>Account eröffnen</button>
         </form>
         {error && <p>Es ist ein Fehler aufgetreten!</p>}
