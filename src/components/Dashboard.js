8931bd6c99fb968a75a2a4919353ca1c900f42b9
Nicolas Ganz <nicolas@keltec.ch>
2018-03-20 13:24:09 +0100

    moved the transaction table to a seperate component

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,7 +1,8 @@
 import React from "react";
-import { Header, Table } from 'semantic-ui-react';
+import { Header } from 'semantic-ui-react';
 import * as api from "../api";
 import TransactionForm from "./TransactionForm";
+import TransactionTable from "./TransactionTable";
 
 export type Props = {};
 
@@ -14,28 +15,7 @@ class Dashboard extends React.Component<Props, *> {
     return (
       <div>
         <Header as="h1">Dashboard</Header>
-        <Table>
-          <Table.Header>
-            <Table.Row>
-              <Table.HeaderCell>Von</Table.HeaderCell>
-              <Table.HeaderCell>An</Table.HeaderCell>
-              <Table.HeaderCell>Betrag</Table.HeaderCell>
-              <Table.HeaderCell>Total</Table.HeaderCell>
-              <Table.HeaderCell>Date</Table.HeaderCell>
-            </Table.Row>
-          </Table.Header>
-          <Table.Body>
-            {this.state.transactions.map((o, i) =>
-              <Table.Row>
-                <Table.Cell>{o.from}</Table.Cell>
-                <Table.Cell>{o.target}</Table.Cell>
-                <Table.Cell>{o.amount}</Table.Cell>
-                <Table.Cell>{o.total}</Table.Cell>
-                <Table.Cell>{o.date}</Table.Cell>
-              </Table.Row>
-            )}
-          </Table.Body>
-        </Table>
+        <TransactionTable transactions={this.state.transactions} />
         <TransactionForm />
       </div>
     );
---
+++ src/components/TransactionTable.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { Table } from 'semantic-ui-react';
+
+export type Props = {
+  transactions: Array,
+};
+
+class TransactionTable extends React.Component<Props, *> {
+  render() {
+    return (
+      <Table>
+        <Table.Header>
+          <Table.Row>
+            <Table.HeaderCell>Von</Table.HeaderCell>
+            <Table.HeaderCell>An</Table.HeaderCell>
+            <Table.HeaderCell>Betrag</Table.HeaderCell>
+            <Table.HeaderCell>Total</Table.HeaderCell>
+            <Table.HeaderCell>Date</Table.HeaderCell>
+          </Table.Row>
+        </Table.Header>
+        <Table.Body>
+          {this.props.transactions.map((o, i) =>
+            <Table.Row>
+              <Table.Cell>{o.from}</Table.Cell>
+              <Table.Cell>{o.target}</Table.Cell>
+              <Table.Cell>{o.amount}</Table.Cell>
+              <Table.Cell>{o.total}</Table.Cell>
+              <Table.Cell>{o.date}</Table.Cell>
+            </Table.Row>
+          )}
+        </Table.Body>
+      </Table>
+    );
+  };
+}
+
+export default TransactionTable;
